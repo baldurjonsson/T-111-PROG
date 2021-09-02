b9b4cbd23d9fca301b3ecee2cfdca729bf0d3ea6
Baldur Jónsson <baldur@helpdesk.is>
2021-09-02 14:46:07 +0000

    Implementing the algorithm.

--- max_int.py
+++ max_int.py
@@ -1,3 +1,11 @@
 # Á meðan input er stærra eða jafnt og núll.
 # berið það saman við max_int. Ef input er stærra en max_int, yfirskrifð sem max_int og endurtakið.
 # Ef input er neikvætt, ljúkið keyrslu.
+
+max_int = 0
+
+while (num_int := int(input("Input a number: "))) >= 0:
+    if num_int > max_int:
+        max_int = num_int
+
+print("The maximum is", max_int)  # Do not change this line
