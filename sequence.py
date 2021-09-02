ca016ba10bd159b0434f1b8357d80d6ecbea6a78
Baldur Jónsson <baldur@helpdesk.is>
2021-09-02 15:01:09 +0000

    Implementing sequence.py

--- sequence.py
+++ sequence.py
@@ -1,3 +1,16 @@
 # Farið yfir tölur frá 0 uppí n
 # Útbúið lista af tölum og bætið einn við í hverri umferð.
 # Talan sem bætist við er summa talnana sem á undan hafa komið.
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+numbers = []
+
+for i in range(n):
+    sum = 0
+    for pn in numbers[-3:]:
+        sum += pn
+    if sum < 2:
+        sum += 1
+    numbers.append(sum)
+    print(sum)
