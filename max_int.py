c20c3ff579fcd92207f18876ad7ed657308361db
Baldur Jónsson <baldur@helpdesk.is>
2021-09-02 14:17:11 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,3 @@
+# Á meðan input er stærra eða jafnt og núll.
+# berið það saman við max_int. Ef input er stærra en max_int, yfirskrifð sem max_int og endurtakið.
+# Ef input er neikvætt, ljúkið keyrslu.
